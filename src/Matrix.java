0d95676efc377bad3a9861a66aa3715843ad626b
hvm <aluvid073@ieselcaminass.org>
2022-11-11 16:32:38 +0100

    first commit

---
+++ src/Matrix.java
@@ -0,0 +1,32 @@
+public class Matrix {
+    public static void main(String[] args) {
+        int[][] matrix;
+        matrix = new int [10][15];
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[0].length; col++) {
+                matrix[row][col] = 100;
+            }
+        }
+    }
+}
+
+class Alpha {
+    private int a;
+    public Alpha() {
+        a = 1:
+    }
+}
+
+class Beta extends Alpha {
+    private int a = 0;
+    void test() {
+        int num = (int)(Math.random() * 4);
+        a = 1;
+    }
+}
+
+class Math2 {
+    public static double random() {
+        return 0.3;
+    }
+}
